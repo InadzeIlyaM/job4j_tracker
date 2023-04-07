79806e922f03f153751cc4867d7663117140c864
InadzeIlyaM <inadze@yahoo.com>
2023-04-07 16:55:39 +0400

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println(this.active);
+        System.out.println(this.message);
+        System.out.println(this.status);
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error mist = new Error(true, 5, "Massage");
+        Error bug = new Error(false, 31, "Clean");
+        Error bad = new Error(false, 541, "Value");
+        error.printInfo();
+        mist.printInfo();
+        bug.printInfo();
+        bad.printInfo();
+    }
+}
